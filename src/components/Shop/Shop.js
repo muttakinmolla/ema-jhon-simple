f848335d7c0406c093a61030bbd3f5455943faac
muttakin1991 <shawonmuttakin@gmail.com>
2022-09-26 00:26:46 +0600

    cart complete

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useEffect } from 'react';
 import { useState } from 'react';
+import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
 import './Shop.css';
 
@@ -15,7 +16,7 @@ const Shop = () => {
     }, []);
 
     const handleAddToCart = (product) => {
-        const newCart =[...cart, product];
+        const newCart = [...cart, product];
         setCart(newCart)
     }
     return (
@@ -31,8 +32,7 @@ const Shop = () => {
 
             </div>
             <div className="cart-container">
-                <h4>Order summary</h4>
-                <p>Selected Item : {cart.length}</p>
+                <Cart cart={cart}></Cart>
             </div>
         </div>
     );
